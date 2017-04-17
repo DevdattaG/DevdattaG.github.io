5e7212fbbbcff202c2997849466fd0f91492c339
DevdattaG <devdatta.godbole777@gmail.com>
2017-04-17 19:22:44 +0530

    Adds dynamic information screen functionality

--- js/app.js
+++ js/app.js
@@ -3,23 +3,18 @@ $(document).ready(function(){
 	if(window.location.hash === "")
 	{
 		$("#backArrow").hide();
-	}
-	//fetchData();
+		$("#homeScreen").show();
+		$("#infoScreen").hide();
+		fetchData();
+	}else if(+window.location.hash.split('#')[1] > 0)
+	{
+		showInfoScreen(window.location.hash.split('#')[1]);
+	}	
 
-	$("#backArrow").click(function(){
-		$("#backArrow").hide();
+	$("#backArrow").click(function(){		
 		window.location = "index.html";
 	});
-
-	// $(".selected").click(function(){
-	// 	$("#backArrow").show();
-	// });
 });
-
-function hideBackArrow()
-{
-	$("#backArrow").show();
-}
 
 function fetchData(){
 	fetch("./data/dataAll.json")
@@ -36,5 +31,32 @@ function fetchData(){
 }
 
 function showData(id,image,name,country){	
-	return '<div class="section"><a onclick=hideBackArrow(); href="#'+id+'"><div class="row"><div class="col-xs-6"><img class= "img-responsive img-rounded" src="./'+image+'" ></div><div class="col-xs-6"></div><p class="placeTitle">'+name+'</p><p class="placeCountry">'+country+'</p></div></a></div>';	
+	return '<div class="section"><a onclick=showInfoScreen('+id+'); href="#'+id+'"><div class="row"><div class="col-xs-6"><img class= "img-responsive img-rounded" src="./'+image+'" ></div><div class="col-xs-6"></div><p class="placeTitle">'+name+'</p><p class="placeCountry">'+country+'</p></div></a></div>';	
+}
+
+function showImageGallery(image){
+	return '<div class="col-xs-12 pad10"><img class= "img-responsive img-rounded" src="./'+image+'" ></div>';
+}
+
+function showInfoScreen(id){
+	$("#backArrow").show();
+	$("#homeScreen").hide();
+	$("#infoScreen").show();
+	var source = "./data/data"+ id +".json"
+	fetch(source)
+	.then(function(response){
+		console.log(response);
+		response.text().then(function(myData){
+			var outputData = JSON.parse(myData);			
+			$("#infoName").html(outputData.name);
+			$("#infoCity").html(outputData.city?outputData.city+", "+outputData.country:outputData.country);
+			$("#infoDescription").html(outputData.description);
+			$("#infoImage").attr("src",outputData.imageUrl);
+			var imageData = "";
+			outputData.imageGallery.forEach(function(val){
+				imageData+=showImageGallery(val);
+			});
+			$("#infoGallery").html(imageData);
+		});
+	});		
 }

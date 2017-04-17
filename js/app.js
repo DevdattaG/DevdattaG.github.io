9366ada067ce13f71adf0d4275a1edd389c4c724
DevdattaG <devdatta.godbole777@gmail.com>
2017-04-17 18:07:40 +0530

    Adds design for information screen

--- js/app.js
+++ js/app.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
 	console.log("app loaded");
-	fetchData();
+	if(window.location.hash === "")
+	{
+		$("#backArrow").hide();
+	}
+	//fetchData();
+
+	$("#backArrow").click(function(){
+		$("#backArrow").hide();
+		window.location = "index.html";
+	});
+
+	// $(".selected").click(function(){
+	// 	$("#backArrow").show();
+	// });
 });
+
+function hideBackArrow()
+{
+	$("#backArrow").show();
+}
 
 function fetchData(){
 	fetch("./data/dataAll.json")
@@ -12,11 +30,11 @@ function fetchData(){
 			JSON.parse(myData).forEach(function(val){
 				outputData += showData(val.id,val.imageUrl,val.name,val.country);
 			});			
-			$("#dataList").html(outputData);
+			$("#homeScreen").html(outputData);
 		});
 	});		
 }
 
 function showData(id,image,name,country){	
-	return '<div class="section"><a href="#'+id+'"><div class="row"><div class="col-xs-6"><img class= "img-responsive img-rounded" src="./'+image+'" ></div><div class="col-xs-6"></div><p class="placeTitle">'+name+'</p><p class="placeCountry">'+country+'</p></div></a></div>';	
+	return '<div class="section"><a onclick=hideBackArrow(); href="#'+id+'"><div class="row"><div class="col-xs-6"><img class= "img-responsive img-rounded" src="./'+image+'" ></div><div class="col-xs-6"></div><p class="placeTitle">'+name+'</p><p class="placeCountry">'+country+'</p></div></a></div>';	
 }

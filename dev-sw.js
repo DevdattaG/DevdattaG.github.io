dca5a434ba68017ce8476686ff560e0b37d0232f
DevdattaG <devdatta.godbole777@gmail.com>
2017-04-18 18:13:05 +0530

    Modifies service worker

--- dev-sw.js
+++ dev-sw.js
@@ -2,17 +2,23 @@
   'use strict';
 
   // Load the sw-tookbox library.
-  importScripts('./node_modules/sw-toolbox/sw-toolbox.js');
+  importScripts('node_modules/sw-toolbox/sw-toolbox.js');
 
   // Turn on debug logging, visible in the Developer Tools' console.
   global.toolbox.options.debug = true;
 
-  toolbox.precache([ './index.html',                        
+  toolbox.precache([ './index.html', 
+                     './index.html#1',
+                     './index.html#2',
+                     './index.html#3',
+                     './index.html#4',
+                     './index.html#5',
+                     './index.html#6',    
                      './js/jquery-1.9.1.min.js',
                      './js/bootstrap.min.js',
                      './styles/bootstrap.min.css',
                      './styles/styles.css',                     
-                     './js/app.js'                     
+                     './js/app.js'
                      ]);
 
   // The route for the images
@@ -29,7 +35,9 @@
       name: 'stylesheets',
       maxEntries: 5,
       maxAgeSeconds: 604800
-    },    
+    },
+    // Set a timeout threshold of 4 seconds
+    networkTimeoutSeconds: 4
   });
 
   toolbox.router.get('./js/(.*)', global.toolbox.cacheFirst, {
@@ -37,7 +45,9 @@
       name: 'scripts',
       maxEntries: 5,
       maxAgeSeconds: 604800
-    },    
+    },
+    // Set a timeout threshold of 4 seconds
+    networkTimeoutSeconds: 4
   });
 
   toolbox.router.get('./data/(.*)', global.toolbox.cacheFirst, {
@@ -57,60 +67,19 @@
   global.addEventListener('activate', event => event.waitUntil(global.clients.claim()));
 })(self);
 
-// this.addEventListener('fetch', event => {
-//   // request.mode = navigate isn't supported in all browsers
-//   // so include a check for Accept: text/html header.
-//   if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-//         event.respondWith(
-//           fetch(event.request.url).catch(error => {
-//               var cachedFile = getFilenameFromUrl(event.request.url);
-//               // Return the offline page
-//               return caches.match(cachedFile);
-//           })
-//     );
-//   }
-// });
-
-// this.addEventListener('fetch', function(event) {
-//   console.log(event.request.url);
-
-//   event.respondWith(
-//     caches.match(event.request).then(function(response) {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
-
-
-this.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function(res){
-            if(res){
-                return res;
-            }
-            requestBackend(event);
-        })
-    )
+this.addEventListener('fetch', event => {
+  // request.mode = navigate isn't supported in all browsers
+  // so include a check for Accept: text/html header.
+  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+        event.respondWith(
+          fetch(event.request.url).catch(error => {
+              var cachedFile = getFilenameFromUrl(event.request.url);
+              // Return the offline page
+              return caches.match(cachedFile);
+          })
+    );
+  }
 });
-
-function requestBackend(event){
-    var url = event.request.clone();
-    return fetch(url).then(function(res){
-        //if not a valid response send the error
-        if(!res || res.status !== 200 || res.type !== 'basic'){
-            return res;
-        }
-
-        var response = res.clone();
-
-        caches.open(CACHE_VERSION).then(function(cache){
-            cache.put(event.request, response);
-        });
-
-        return res;
-    })
-}
-
 
 function getFilenameFromUrl(path){
     path = path.substring(path.lastIndexOf("/")+ 1);
